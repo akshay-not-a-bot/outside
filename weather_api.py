00c4cd25e6b410ae048663a13744d677643ceff6
Akshay Patel <akshaypatel2710@gmail.com>
2025-03-01 00:29:44 +0530

    Raw weather data fetching working Next: Pretify the raw data, Agenda: Handle hourly data and forecast

--- weather_api.py
+++ weather_api.py
@@ -1,8 +1,12 @@
 import requests
 import json
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
 
 
 def get_cordinates(city: str):
+    # geocoding API calling
     url = "https://geocoding-api.open-meteo.com/v1/search"
     params = {"name": city, "count": 10, "language": "en", "format": "json"}
 
@@ -39,9 +43,50 @@ def get_cordinates(city: str):
         print(f"Enter only the index of the city: {TE}")
 
 
+def get_weather(cords):
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
+
+    # weather API calling
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": cords[0],
+        "longitude": cords[1],
+        "current": ["temperature_2m", "is_day", "precipitation", "wind_speed_10m"],
+        "hourly": ["temperature_2m", "precipitation", "wind_speed_10m", "is_day"],
+    }
+
+    try:
+        responses = openmeteo.weather_api(url, params=params)
+        response = responses[0]
+
+        # Current values. The order of variables needs to be the same as requested.
+        current = response.Current()
+        current_temperature_2m = current.Variables(0).Value()
+        current_is_day = current.Variables(1).Value()
+        current_precipitation = current.Variables(2).Value()
+        current_wind_speed_10m = current.Variables(3).Value()
+
+        # printing data:
+        print(f"Current time {current.Time()}")
+        print(f"Current temperature_2m {current_temperature_2m}")
+        print(f"Current is_day {current_is_day}")
+        print(f"Current precipitation {current_precipitation}")
+        print(f"Current wind_speed_10m {current_wind_speed_10m}")
+
+    except Exception as E:
+        print(f"Error while fetching weather data: {E}")
+
+        # NOT HANDLING HOURLY DATA AND FORECAST YET
+
+
 def main():
     city = input("Enter city name: ").strip()
     cords = get_cordinates(city)
+    print(cords)
+    weather_data = get_weather(cords)
 
 
 if __name__ == "__main__":
